392a23160606b4f9186d87bf0bfa7c8ba73da6e7
dconnoll <connollydillon@gmail.com>
2016-04-19 21:14:08 -0400

    Added dialog fragment
    feat: Added dialog fragment.
    fix: none.
    docs: none.

--- app/src/main/java/com/conz13/d/strongpasswordcreator/MainActivity.java
+++ app/src/main/java/com/conz13/d/strongpasswordcreator/MainActivity.java
@@ -12,13 +12,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.temp_layout);
+        setContentView(R.layout.main_activity_container);
 
         if(savedInstanceState == null){
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new TempFragment())
+                    .add(R.id.main_container, new TempFragment())
                     .commit();
         }
     }
-
 }
---
+++ app/src/main/java/com/conz13/d/strongpasswordcreator/PasswordPromptDialogFragment.java
@@ -0,0 +1,13 @@
+package com.conz13.d.strongpasswordcreator;
+
+import android.app.DialogFragment;
+
+/**
+ * Created by dillon on 4/19/16.
+ *
+ * TODO: Create based on the google guidelines for an AlertDialog using custom layout
+ * reference link: http://developer.android.com/guide/topics/ui/dialogs.html
+ */
+public class PasswordPromptDialogFragment extends DialogFragment {
+
+}
