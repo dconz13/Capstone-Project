a1b767f2ad9d17a4d6fdb22f733d9a6b60da01af
dconnoll <connollydillon@gmail.com>
2016-04-21 14:06:14 -0400

    Functioning Nav Drawer
    feat: Added navigation drawer to main activity. OnItemClick isn't handled yet.
    fix: none
    docs: none

--- app/src/main/java/com/conz13/d/strongpasswordcreator/MainActivity.java
+++ app/src/main/java/com/conz13/d/strongpasswordcreator/MainActivity.java
@@ -2,22 +2,77 @@ package com.conz13.d.strongpasswordcreator;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 /**
  * Created by dillon on 4/17/16.
  */
 public class MainActivity extends AppCompatActivity {
 
+    private String[] tempList = {"item1","item2","item3","item4"};
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private ActionBarDrawerToggle mDrawerToggle;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_activity_container);
+        setContentView(R.layout.main_activity_layout);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.nav_drawer_layout);
+        Toolbar tempToolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, tempToolbar,
+                R.string.drawer_open,R.string.drawer_close);
+        mDrawerList = (ListView) findViewById(R.id.drawer_list);
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, tempList));
+
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        setSupportActionBar(tempToolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
 
         if(savedInstanceState == null){
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.main_container, new TempFragment())
+                    .add(R.id.content_frame, new WordGenerationFragment())
                     .commit();
         }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(mDrawerLayout.isDrawerVisible(GravityCompat.START)){
+            mDrawerLayout.closeDrawers();
+        }else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
     }
 }
--- app/src/main/java/com/conz13/d/strongpasswordcreator/WordGenerationFragment.java
+++ app/src/main/java/com/conz13/d/strongpasswordcreator/WordGenerationFragment.java
@@ -1,7 +1,6 @@
 package com.conz13.d.strongpasswordcreator;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,11 +9,11 @@ import android.view.ViewGroup;
 /**
  * Created by dillon on 4/18/16.
  */
-public class TempFragment extends Fragment {
+public class WordGenerationFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.temp_fragment_layout, container, false);
+        View rootView = inflater.inflate(R.layout.word_generation_fragment_layout, container, false);
 
         return rootView;
     }
